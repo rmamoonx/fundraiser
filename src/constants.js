c39a38feaf3e3868aade8bedeb969566038cf2e6
Mamoon Raja <59180384+rmamoonx@users.noreply.github.com>
2021-01-31 17:50:05 +0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ import heroimage from "./page/karl-fredrickson-192686.jpg";
 
 const AppConstants = {
     STRIPE_PK_TEST: "pk_test_yourtestpublishablekey",
-    STRIPE_PK_PROD: "pk_yourprodpublishablekey",,
+    STRIPE_PK_PROD: "pk_yourprodpublishablekey",
     STRIPE_TEST_MODE: true, // Set this to false to stop using the Stripe testing environment
     PAYMENT_SERVER_URL: "http://localhost:3001",
     HERO_TITLE: "Help send these children to college",
